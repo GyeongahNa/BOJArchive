cfc182208d136b5686d5178a5886a0227ddf8b9b
GyeongahNa <gyeongahna@naver.com>
2022-10-06 11:40:33 +0900

    모눈종이자르기 수정

--- code/2538_모눈종이자르기.cpp
+++ code/2538_모눈종이자르기.cpp
@@ -21,78 +21,102 @@ using llll = pair<ll, ll>;
 #define X first
 #define Y second
 
-int M, N, K;
-ll mx;
-vector<llll> coords, points;
+ll M, N, K;
+vector<ii> coords, points;
 
 void getInput() {
 
     cin >> M >> N >> K;
     for (int i=0; i<K; i++) {
-        ll y, x;
+        int y, x;
         cin >> y >> x;
         coords.push_back({N-x, y});
     }
 
-    for (int i=0; i<K; i++) 
+    for (int i=0; i<K; i++)
         coords.push_back(coords[i]);
 
-    points = {{0, M}, {0, 0}, {N, 0}, {N, M}};
+    points = {{0, 0}, {N, 0}, {N, M}, {0, M}};
 }
 
-ll line(const llll& coord) {
+vector<int> line(const ii& coord) {
 
-    ll x = coord.X, y = coord.Y;
-    if (y == M) return 0;
-    if (x == 0) return 1;
-    if (y == 0) return 2;
-    if (x == N) return 3;
-    return -1;
+    vector<int> ret;
+    int x = coord.X, y = coord.Y;
+
+    if (x == 0) ret.push_back(0);
+    if (y == 0) ret.push_back(1);
+    if (x == N) ret.push_back(2);
+    if (y == M) ret.push_back(3);
+    return ret;
+}
+
+bool noLine() {
+
+    int nv = 0;
+    for (int i=0; i<K; i++) {
+        if (line(coords[i]).size()) nv++;
+    }
+    return nv == 0;
+}
+
+bool sameLine(const ii& coord1, const ii& coord2) {
+
+    vector<int> l1 = line(coord1);
+    vector<int> l2 = line(coord2);
+
+    for (auto x : l1) {
+        for (auto y: l2) {
+            if (x == y) return true;
+        }
+    }
+    return false;
 }
 
 int main() {
 
-    // getInput();
+    getInput();
 
-    // int nv = 0;
-    // for (int i=0; i<K; i++) {
-    //     if (line(coords[i]) == -1) continue;
-    //     nv++;
-    // }
+    //모든 점이 내부에 있을 때 
+    if (noLine()) {
+        ll ans = 0;
+        for (int i=0; i<K; i++) 
+            ans += abs(coords[i].X-coords[i+1].X)+abs(coords[i].Y-coords[i+1].Y);
+        cout<<1<<" "<<2*(N+M)+ans;
+        return 0;
+    }
 
-    // if (!nv) {
-    //     ll ans = 0;
-    //     for (int i=0; i<K; i++)
-    //         ans += abs(coords[i].X-coords[i+1].X) + abs(coords[i].Y-coords[i+1].Y);
-    //     cout<<1<<" "<<2*(N+M)+ans;
-    //     return 0;
-    // }
-
-    int cnt = 0;
+    ll cnt = 0, mx = 0;
     int sz = (int)coords.size();
 
     for (int s=0; s<K; s++) {
-        int l = line(coords[s]);
-        if (l == -1) continue;
-        else if (s+1 >= sz || l == line(coords[s+1])) continue;
+        vector<int> l = line(coords[s]);
+        if (!l.size()) continue;
 
         int e;
-        for (e = s+1; e<sz && line(coords[e]) == -1; e++) {}
+        for (e = s+1; e<sz && !line(coords[e]).size(); e++) {}
+        if (s+1 == e && sameLine(coords[s], coords[e])) continue;
 
+        //조각의 내부 테두리 합
         ll sum = 0;
-        for (int i=s+1; i<=e; i++)
-            sum += abs(coords[i-1].X-coords[i].X) + abs(coords[i-1].Y-coords[i].Y);
+        for (int i=s+1; i<=e; i++) 
+            sum += abs(coords[i].X-coords[i-1].X)+ abs(coords[i].Y-coords[i-1].Y);
 
-        int lines = line(coords[s]);
-        int linee = line(coords[e]);
+        int lines = line(coords[s])[0];
+        int linee = line(coords[e])[0];
         int x = coords[s].X, y = coords[s].Y;
-        if (linee < lines) linee += 4;
 
-        for (int i=lines; i<=linee; i++) {
-            if (i == linee) sum += abs(x-coords[e].X)+abs(y-coords[e].Y);
-            else sum += abs(x-points[i%4].X)+abs(y-points[i%4].Y);
-            x = points[i%4].X;
-            y = points[i%4].Y;
+        //조각의 외부 테두리 합
+        for (int i=lines; i<lines+4; i++) {
+            int d = i%4;
+            if (d != linee) {
+                sum += abs(x-points[d].X)+abs(y-points[d].Y);
+                x = points[d].X;
+                y = points[d].Y;
+                continue;
+            }
+            sum += abs(x-coords[e].X)+abs(y-coords[e].Y);
+            break;
         }
 
         s = e;
