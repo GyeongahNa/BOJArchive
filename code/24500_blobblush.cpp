b1d49559c2ba2c7aa19dfad23cd98426b5433f1e
GyeongahNa <gyeongahna@naver.com>
2023-02-05 16:47:27 +0900

    blobblush

--- code/24500_blobblush.cpp
+++ code/24500_blobblush.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 #include <cstring>
-#include <cmath>
 #include <algorithm>
 #include <tuple>
 #include <vector>
@@ -18,55 +17,24 @@ using iii = tuple<int, int, int>;
 #define X first
 #define Y second
 
-int N;
-
-string toBin(int x) {
-
-    string re = "";
-
-    while (x) {
-        re = string(1, x%2+'0') + re;
-        x /= 2;
-    }
-    return re;
-}
+long long N;
 
 int main() {
 
     cin >> N;
 
-
-    string s = toBin(N);
-
-    cout << "s : " << s << endl;
-
-    string ns = "";
-    for (auto &e : s) {
-        ns += (e == '1' ? "0" : "1");
+    long long x = 1, A = 0;
+    while (true) {
+        A = (1LL<<x)-1;
+        if (A >= N) break;
+        x++;
     }
 
-    cout << "ns : " << ns << endl;
-
-    int nn = 0;
-    for (int i=0; i<; i--) {
-        if (ns[i] == '0') continue;
-        cout << "i : " << i << endl;
-        cout << "i-(ns.size()-1): " << i-(ns.size()-1) << endl; 
-         nn += (1<<(i-(ns.size()-1)));
+    if (A == N) {
+        cout << 1 << '\n' << N;
+        return 0;
     }
 
-    cout << "nn: " << nn << endl;
-
-    if (stoi(ns) == 0) {
-        cout << 1 << '\n';
-        cout << N << '\n';
-    }
-    else {
-        cout << 2 << '\n';
-        cout << N << '\n';
-        cout << stoi(ns) << '\n';
-    }
-
-
-
+    cout << 2 << '\n';
+    cout << (N^A) << '\n' << N;
 }
